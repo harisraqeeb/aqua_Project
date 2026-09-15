cc39506409eb7412a5af00014e0d0a6d4229d5db
“harisraqeeb” <harisrajaharisraja902@gmail.com>
2026-09-15 16:29:40 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,8 @@ const AnimationObserver = () => {
     };
 
     const observerOptions = {
-      threshold: 0.1,
-      rootMargin: "0px 0px -40px 0px",
+      threshold: 0.05,
+      rootMargin: "0px 0px 0px 0px",
     };
 
     const observer = new IntersectionObserver(observerCallback, observerOptions);
